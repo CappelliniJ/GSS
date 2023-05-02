64cbef14a5255c77189deb084e7a428e2f0878a1
Justin S Cappellini <cappellinij@gmail.com>
2023-05-03 02:04:01 +0800

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
 const config = {
   logging: false
@@ -11,15 +11,26 @@ if(process.env.LOGGING === 'true'){
   delete config.logging
 }
 
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
+let connectionString;
+
 if(process.env.DATABASE_URL){
+  connectionString = process.env.DATABASE_URL;
   config.dialectOptions = {
     ssl: {
       rejectUnauthorized: false
     }
   };
+} else {
+  const dbUsername = 'grace_buster_pa7j_user';
+  const dbPassword = 'WGxj6k7Ms3Lygmo0oUIuvKXdDClxsvrA';
+  const dbHost = 'dpg-ch8krs9mbg54hi5tr03g-a';
+  const dbName = 'grace_buster_pa7j';
+  
+  connectionString = `postgres://${dbUsername}:${dbPassword}@${dbHost}/${dbName}`;
 }
 
-const db = new Sequelize('postgres://grace_buster_pa7j_user:WGxj6k7Ms3Lygmo0oUIuvKXdDClxsvrA@dpg-ch8krs9mbg54hi5tr03g-a/grace_buster_pa7j', config);
+const db = new Sequelize(connectionString, config);
+
 module.exports = db;
+
 
